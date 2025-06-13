eb426795d93c22dce04d3ed64a77b73dfe21dab9
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-06-13 09:31:58 -0400

    Démo Validation Client

--- DemoFormulaires/Models/CodeBarreValideAttribute.cs
+++ DemoFormulaires/Models/CodeBarreValideAttribute.cs
@@ -1,20 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
-using ModelMetadata = System.Web.Mvc.ModelMetadata;
-using ControllerContext = System.Web.Mvc.ControllerContext;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 
 namespace DemoFormulaires.Models
 {
 
-    public class CodeBarreValideAttribute : ValidationAttribute, IClientModelValidator //, IClientValidatable
+    public class CodeBarreValideAttribute : ValidationAttribute, IClientModelValidator
     {
         public override bool IsValid(object value)
         {
             var str = value.ToString();
+            // = ErrorMessage si ErrorMessage existe ou = "..." sinon
             ErrorMessage = ErrorMessage ?? "Le code-barres doit commencer par 100 et contenir 8 chiffres.";
             return str.StartsWith("100") && str.Length == 8;
         }
@@ -23,30 +19,10 @@ namespace DemoFormulaires.Models
         public void AddValidation(ClientModelValidationContext context)
         {
             context.Attributes.Add("data-val", "true");
-            context.Attributes.Add("data-val-codebarrevalide", /*ErrorMessage ??*/ "Le code-barres doit commencer par 1000 et contenir 8 chiffres.");
+            //Ajouter l'attribut "data-val-codebarrevalide" pour lier à la validation JQuery
+            context.Attributes.Add("data-val-codebarrevalide", /*ErrorMessage ??*/ 
+                "Le code-barres doit commencer par 1000 et contenir 8 chiffres.");
         }
 
-
-        //PAS COMPATIBLE AVEC .NET CORE
-        /*
-        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
-        {
-
-            yield return new ModelClientValidationRule
-            {
-                ValidationType = "codebarrevalide", 
-                ErrorMessage = FormatErrorMessage(metadata.DisplayName)
-            };
-            /*
-            List<ModelClientValidationRule> liste = new List<ModelClientValidationRule>();
-            liste.Add(new ModelClientValidationRule()
-            {
-                ValidationType = "codebarrevalide",
-                ErrorMessage = FormatErrorMessage(metadata.DisplayName)
-            });
-            return liste;
-            */
-        }
-        */
     }
 }
--- DemoFormulaires/Models/Produit.cs
+++ DemoFormulaires/Models/Produit.cs
@@ -8,6 +8,7 @@ namespace DemoFormulaires.Models
 
         [Required(ErrorMessage ="Ce champ est obligatoire.")]
         [Display(Name = "Code barre")]
+        [Length(8, 8)]
         //[Range(10000000, long.MaxValue, ErrorMessage = "Le code barre doit avoir au moins 8 chiffres")]
         [CodeBarreValide(ErrorMessage = "M: Le code-barres doit commencer par 100 et contenir 8 chiffres.")]
         public string CodeBarre { get; set; }
